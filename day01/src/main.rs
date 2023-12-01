d02fc1f5f6fbeed639ae43ceed62b17eaa05952a
Mathieu Lemay <acidrain1@gmail.com>
2023-12-01 16:25:02 -0500

    Day 01: Optimize by getting only first and last char

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -4,9 +4,9 @@ use std::time::Instant;
 use aoc_common::get_input;
 
 fn solve(input: &[String]) -> (impl Display, impl Display) {
-    let numbers = extract_digits(input, false);
+    let numbers = extract_first_and_last_digits(input, false);
     let p1 = get_calibration_value(&numbers);
-    let numbers = extract_digits(input, true);
+    let numbers = extract_first_and_last_digits(input, true);
     let p2 = get_calibration_value(&numbers);
 
     (p1, p2)
@@ -26,57 +26,108 @@ fn main() {
     println!("Duration: {:.3}μs", t);
 }
 
-fn extract_digits(input: &[String], include_spelled_out: bool) -> Vec<Vec<u32>> {
+fn extract_first_and_last_digits(input: &[String], include_spelled_out: bool) -> Vec<(u32, u32)> {
     let mut all_digits = Vec::with_capacity(input.len());
 
     for entry in input {
-        let mut digits_of_entry = Vec::new();
+        let mut first: Option<u32> = None;
+        let mut last: Option<u32> = None;
 
         for (i, c) in entry.chars().enumerate() {
             if c.is_numeric() {
-                digits_of_entry.push(c.to_digit(10).unwrap());
-                continue;
+                first = Some(c.to_digit(10).unwrap());
+                break;
             }
 
             if !include_spelled_out {
                 continue;
             }
 
-            if entry[i..].starts_with("one") {
-                digits_of_entry.push(1);
-            } else if entry[i..].starts_with("two") {
-                digits_of_entry.push(2);
-            } else if entry[i..].starts_with("three") {
-                digits_of_entry.push(3);
-            } else if entry[i..].starts_with("four") {
-                digits_of_entry.push(4);
-            } else if entry[i..].starts_with("five") {
-                digits_of_entry.push(5);
-            } else if entry[i..].starts_with("six") {
-                digits_of_entry.push(6);
-            } else if entry[i..].starts_with("seven") {
-                digits_of_entry.push(7);
-            } else if entry[i..].starts_with("eight") {
-                digits_of_entry.push(8);
-            } else if entry[i..].starts_with("nine") {
-                digits_of_entry.push(9);
+            let substr = &entry[i..];
+
+            if substr.starts_with("one") {
+                first = Some(1);
+                break;
+            } else if substr.starts_with("two") {
+                first = Some(2);
+                break;
+            } else if substr.starts_with("three") {
+                first = Some(3);
+                break;
+            } else if substr.starts_with("four") {
+                first = Some(4);
+                break;
+            } else if substr.starts_with("five") {
+                first = Some(5);
+                break;
+            } else if substr.starts_with("six") {
+                first = Some(6);
+                break;
+            } else if substr.starts_with("seven") {
+                first = Some(7);
+                break;
+            } else if substr.starts_with("eight") {
+                first = Some(8);
+                break;
+            } else if substr.starts_with("nine") {
+                first = Some(9);
+                break;
             }
         }
 
-        all_digits.push(digits_of_entry)
+        for (i, c) in entry.chars().rev().enumerate() {
+            if c.is_numeric() {
+                last = Some(c.to_digit(10).unwrap());
+                break;
+            }
+
+            if !include_spelled_out {
+                continue;
+            }
+
+            let substr = &entry[entry.len() - i - 1..];
+
+            if substr.starts_with("one") {
+                last = Some(1);
+                break;
+            } else if substr.starts_with("two") {
+                last = Some(2);
+                break;
+            } else if substr.starts_with("three") {
+                last = Some(3);
+                break;
+            } else if substr.starts_with("four") {
+                last = Some(4);
+                break;
+            } else if substr.starts_with("five") {
+                last = Some(5);
+                break;
+            } else if substr.starts_with("six") {
+                last = Some(6);
+                break;
+            } else if substr.starts_with("seven") {
+                last = Some(7);
+                break;
+            } else if substr.starts_with("eight") {
+                last = Some(8);
+                break;
+            } else if substr.starts_with("nine") {
+                last = Some(9);
+                break;
+            }
+        }
+
+        let first = first.expect("string has no digit.");
+        let last = last.expect("string has no digit.");
+
+        all_digits.push((first, last))
     }
 
     all_digits
 }
 
-fn get_calibration_value(entries: &[Vec<u32>]) -> u32 {
-    let mut sum: u32 = 0;
-    for digits in entries.iter() {
-        sum += (*digits.first().unwrap()) * 10;
-        sum += *digits.last().unwrap();
-    }
-
-    sum
+fn get_calibration_value(entries: &[(u32, u32)]) -> u32 {
+    entries.iter().map(|e| e.0 * 10 + e.1).sum()
 }
 
 #[cfg(test)]
@@ -96,7 +147,7 @@ mod tests {
             ",
         );
 
-        let digits = extract_digits(&input, false);
+        let digits = extract_first_and_last_digits(&input, false);
         let res = get_calibration_value(&digits);
 
         assert_eq!(res, 142);
@@ -116,7 +167,7 @@ mod tests {
             ",
         );
 
-        let digits = extract_digits(&input, true);
+        let digits = extract_first_and_last_digits(&input, true);
         let res = get_calibration_value(&digits);
 
         assert_eq!(res, 281);
@@ -126,7 +177,7 @@ mod tests {
     fn test_p1_full_input() {
         let input = get_input("day01.txt");
 
-        let digits = extract_digits(&input, false);
+        let digits = extract_first_and_last_digits(&input, false);
         let res = get_calibration_value(&digits);
 
         assert_eq!(res, 56049);
@@ -136,7 +187,7 @@ mod tests {
     fn test_p2_full_input() {
         let input = get_input("day01.txt");
 
-        let digits = extract_digits(&input, true);
+        let digits = extract_first_and_last_digits(&input, true);
         let res = get_calibration_value(&digits);
 
         assert_eq!(res, 54530);
