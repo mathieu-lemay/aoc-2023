2c2a7b2ebaa82cb2072937d656217c32e49733c7
Mathieu Lemay <acidrain1@gmail.com>
2023-12-06 08:52:07 -0500

    Day 06

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -2,6 +2,7 @@ use std::fmt::Display;
 use std::time::Instant;
 
 use aoc_common::get_input;
+use itertools::Itertools;
 
 fn main() {
     let input = get_input("day06.txt");
@@ -17,11 +18,69 @@ fn main() {
     println!("Duration: {:.3}μs", t);
 }
 
-fn solve(_input: &[String]) -> (impl Display, impl Display) {
-    let p1 = 0;
-    let p2 = 0;
+fn solve(input: &[String]) -> (impl Display, impl Display) {
+    let races = parse_races(input);
+    let p1 = get_error_margin(&races);
+
+    let race = parse_race(input);
+    let p2 = race.get_number_of_winning_strategies();
 
     (p1, p2)
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Race {
+    time: u64,
+    record: u64,
+}
+
+impl Race {
+    fn get_number_of_winning_strategies(&self) -> usize {
+        (0..=self.time)
+            .filter(|t| t * (self.time - t) > self.record)
+            .count()
+    }
+}
+
+fn parse_races(input: &[String]) -> Vec<Race> {
+    let times: Vec<u64> = input[0][9..]
+        .split(' ')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+    let records: Vec<u64> = input[1][9..]
+        .split(' ')
+        .filter_map(|s| s.parse().ok())
+        .collect();
+
+    times
+        .iter()
+        .zip_eq(records)
+        .map(|(&time, record)| Race { time, record })
+        .collect()
+}
+
+fn parse_race(input: &[String]) -> Race {
+    let time = input[0]
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>()
+        .parse()
+        .unwrap();
+    let record = input[1]
+        .chars()
+        .filter(|c| c.is_ascii_digit())
+        .collect::<String>()
+        .parse()
+        .unwrap();
+
+    Race { time, record }
+}
+
+fn get_error_margin(races: &[Race]) -> usize {
+    races
+        .iter()
+        .map(|r| r.get_number_of_winning_strategies())
+        .product()
 }
 
 #[cfg(test)]
@@ -33,7 +92,12 @@ mod tests {
 
     #[fixture]
     fn test_input() -> Vec<String> {
-        parse_test_input("")
+        parse_test_input(
+            "
+            Time:      7  15   30
+            Distance:  9  40  200
+        ",
+        )
     }
 
     #[fixture]
@@ -42,30 +106,62 @@ mod tests {
     }
 
     #[rstest]
-    fn test_p1(test_input: Vec<String>) {
-        let res = 0;
+    fn test_parse_races(test_input: Vec<String>) {
+        let races = parse_races(&test_input);
 
-        assert_eq!(res, 1);
+        let expected = vec![
+            Race { time: 7, record: 9 },
+            Race {
+                time: 15,
+                record: 40,
+            },
+            Race {
+                time: 30,
+                record: 200,
+            },
+        ];
+
+        assert_eq!(races, expected);
+    }
+
+    #[rstest]
+    fn test_parse_race(test_input: Vec<String>) {
+        let race = parse_race(&test_input);
+
+        assert_eq!(
+            race,
+            Race {
+                time: 71530,
+                record: 940200
+            }
+        );
+    }
+
+    #[rstest]
+    fn test_p1(test_input: Vec<String>) {
+        let races = parse_races(&test_input);
+
+        assert_eq!(get_error_margin(&races), 288);
     }
 
     #[rstest]
     fn test_p1_full_input(puzzle_input: Vec<String>) {
-        let res = 0;
+        let races = parse_races(&puzzle_input);
 
-        assert_eq!(res, 1);
+        assert_eq!(get_error_margin(&races), 114400);
     }
 
     #[rstest]
     fn test_p2(test_input: Vec<String>) {
-        let res = 0;
+        let race = parse_race(&test_input);
 
-        assert_eq!(res, 1);
+        assert_eq!(race.get_number_of_winning_strategies(), 71503);
     }
 
     #[rstest]
     fn test_p2_full_input(puzzle_input: Vec<String>) {
-        let res = 0;
+        let race = parse_race(&puzzle_input);
 
-        assert_eq!(res, 1);
+        assert_eq!(race.get_number_of_winning_strategies(), 21039729);
     }
 }
